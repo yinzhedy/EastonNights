8964531c5a4266fbd0bbc09e0544fdd968797af1
yinzhedy <yinzhe.dy@gmail.com>
2023-10-17 18:02:53 -0400

    created  variable to dynamically pull the version # of the theme from the top of style.css file

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
 <?php
 
 function eastonnights_register_styles(){
-    wp_enqueue_style('eastonnights-style' , get_template_directory_uri()."/style.css" , array() , '1.0' , 'all' );
+
+    $version = wp_get_theme()->get('Version');
+
+    wp_enqueue_style('eastonnights-style' , get_template_directory_uri()."/style.css" , array() , $version , 'all' );
 }
 
 add_action('wp_enqueue_scripts', 'eastonnights_register_styles');
