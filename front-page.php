6a803ac20f4306417cca2ff19fe692fa25dac7b5
yinzhedy <yinzhe.dy@gmail.com>
2023-10-17 18:17:29 -0400

    created eastonnights_register_scripts() function in functions.php to load main.js script file in assets/js inside of front-page.php

--- front-page.php
+++ front-page.php
@@ -2,36 +2,42 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="Peter Ydeen - Easton Nights Inc.">
-  <meta name="author" content="https://github.com/yinzhedy">
-  <title>Peter Ydeen - Easton Nights Inc.</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Peter Ydeen - Easton Nights Inc.">
+    <meta name="author" content="https://github.com/yinzhedy">
+    <title>Peter Ydeen - Easton Nights Inc.</title>
 
-  <!-- CSS stylesheets -->
-  <link rel="stylesheet" href="style.css">
+    <!-- CSS stylesheets -->
+    <link rel="stylesheet" href="style.css">
 
-  <!-- Optional: Bootstrap or other CSS frameworks -->
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
+    <!-- Optional: Bootstrap or other CSS frameworks -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
 
-  <!-- Favicon -->
-  <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <!-- Favicon -->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 
-  <!-- Additional metadata or styles -->
-  <meta name="description" content="Your website description">
-  <meta name="keywords" content="your, keywords, here">
+    <!-- Additional metadata or styles -->
+    <meta name="description" content="Your website description">
+    <meta name="keywords" content="your, keywords, here">
 
-  <!-- External scripts (optional) -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- External scripts (optional) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-  <!-- Your custom JavaScript file -->
-  <?php
-  wp_head();
-  ?>
+    <!-- Your custom JavaScript file -->
+    <?php
+    wp_head();
+    ?>
 </head>
 
 <body>
-    <div>'Hello'</div>
-</body>
 
+    <div>'Hello'</div>
+
+
+    <?php
+    wp_footer();
+    ?>
+
+</body>
 </html>
--- functions.php
+++ functions.php
@@ -9,4 +9,13 @@ function eastonnights_register_styles(){
 
 add_action('wp_enqueue_scripts', 'eastonnights_register_styles');
 
+function eastonnights_register_scripts(){
+
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_script('eastonnights-script' , get_template_directory_uri()."/assets/js/main.js" , array() , $version , true );
+}
+
+add_action('wp_enqueue_scripts', 'eastonnights_register_scripts');
+
 ?>
+
