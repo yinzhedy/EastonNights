69cfa98ffec8712e5e212583ac6a3ab1fa5a7d5d
yinzhedy <yinzhe.dy@gmail.com>
2023-10-18 14:29:35 -0400

    query for gallery posts was incorrect, now fixed to query using slug to ensure we are displaying the correct gallery the user has browser pathed to.

--- template-parts/content-gallery.php
+++ template-parts/content-gallery.php
@@ -1,11 +1,18 @@
 <?php
-$query = new WP_Query(array('post_type' => 'gallery'));
+// Get the slug from the URL
+$slug = basename(get_permalink());
+
+// Set up the query to retrieve the gallery post based on the slug
+$query = new WP_Query(array(
+    'post_type' => 'gallery',
+    'name' => $slug  // Use the slug to retrieve the specific gallery post
+));
 
 if ($query->have_posts()) :
     while ($query->have_posts()) : $query->the_post();
 ?>
         <h2><?php the_title(); ?></h2>
-        <div class="gallery-title"><?php get_post_field('title'); ?></div>
+        <div class="gallery-title"><?php get_post_field('gallery_title'); ?></div>
 
         <?php
         $gallery_images = get_post_field('gallery_image');
