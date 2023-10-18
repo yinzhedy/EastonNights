e6af1d0ae84dca210091635019a2551e5d2bee91
yinzhedy <yinzhe.dy@gmail.com>
2023-10-17 21:51:22 -0400

    moved if have_posts() function into single post file (single.php) and out of front-page.php

--- front-page.php
+++ front-page.php
@@ -3,15 +3,6 @@
 ?>
 
     <main>
-    <?php
-        if( have_posts() ) {
-            while( have_posts() ) {
-                the_post();
-                the_content();
-            }
-        }
-        
-    ?>
     </main>
 
 <?php
--- single.php
+++ single.php
@@ -1,1 +1,25 @@
 //displaying single blog posts
+<?php
+get_header();
+?>
+
+
+
+<article>
+    <?php
+        if( have_posts() ) {
+            while( have_posts() ) {
+                the_post();
+                the_content();
+            }
+        }
+        
+    ?>
+</article>
+
+
+
+
+<?php
+get_footer();
+?>
